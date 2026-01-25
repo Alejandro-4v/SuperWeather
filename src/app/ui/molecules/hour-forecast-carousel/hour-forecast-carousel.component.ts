08b03522621d32dd84ce3ad3ff6b54c06304ff88
Alejandro-4v <ald@MBP13.local>
2026-01-25 23:30:43 +0100

    feat: hour forecast carousel updated for a more intuitive experience, separated by days with indicators

--- src/app/ui/molecules/hour-forecast-carousel/hour-forecast-carousel.component.ts
+++ src/app/ui/molecules/hour-forecast-carousel/hour-forecast-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, computed } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { ForecastInfo } from '@shared/interfaces/forecast.interfaces';
 import { VerticalHourForecastComponent } from '../vertical-hour-forecast/vertical-hour-forecast.component';
@@ -14,6 +14,27 @@ import { VerticalHourForecastComponent } from '../vertical-hour-forecast/vertica
 export class HourForecastCarouselComponent {
   forecasts = input.required<ForecastInfo[]>();
   title = input<string>("");
+  lang = input<string>('en-GB');
+
+  groupedForecasts = computed(() => {
+    const list = this.forecasts();
+    if (!list || list.length === 0) return [];
+
+    const groups: { day: string, items: ForecastInfo[] }[] = [];
+    list.forEach(item => {
+      const dayName = this.datePipe.transform(item.dt * 1000, 'EEEE', '', this.lang()) || '';
+      const dayDate = this.datePipe.transform(item.dt * 1000, 'd MMM', '', this.lang()) || '';
+      const dayLabel = `${dayName}, ${dayDate}`;
+
+      const lastGroup = groups[groups.length - 1];
+      if (lastGroup && lastGroup.day === dayLabel) {
+        lastGroup.items.push(item);
+      } else {
+        groups.push({ day: dayLabel, items: [item] });
+      }
+    });
+    return groups;
+  });
 
   constructor(private datePipe: DatePipe) { }
 
