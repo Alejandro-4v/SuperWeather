bd6e3b60286b0d01756ac382832443241104d627
Alejacks <ald@MBP13.local>
2026-01-25 12:55:45 +0100

    feat: Searchbar molecule added

---
+++ src/app/ui/molecules/search-bar/search-bar.component.ts
@@ -0,0 +1,28 @@
+import { Component, input, output, signal } from '@angular/core';
+import { InputFieldComponent } from '../../atoms/input-field/input-field.component';
+import { SearchButtonComponent } from '../../atoms/search-button/search-button.component';
+
+@Component({
+    selector: 'app-search-bar',
+    templateUrl: './search-bar.component.html',
+    styleUrls: ['./search-bar.component.scss'],
+    standalone: true,
+    imports: [InputFieldComponent, SearchButtonComponent]
+})
+export class SearchBarComponent {
+    placeholder = input<string>('search_bar_placeholder');
+    value = input<string>('');
+    ariaLabel = input<string>('Search Bar');
+
+    search = output<string>();
+
+    currentSearchValue = signal<string>('');
+
+    onValueChange(value: string) {
+        this.currentSearchValue.set(value);
+    }
+
+    onSearch() {
+        this.search.emit(this.currentSearchValue());
+    }
+}
