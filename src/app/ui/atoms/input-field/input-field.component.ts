f03efff57853bd9f78b4fe7eeb2c3fbb38514516
Alejandro-4v <ald@MBP13.local>
2026-01-25 23:29:44 +0100

    fix: missing html and ts properties for input-field (enter) event added

--- src/app/ui/atoms/input-field/input-field.component.ts
+++ src/app/ui/atoms/input-field/input-field.component.ts
@@ -12,6 +12,7 @@ export class InputFieldComponent {
   ariaLabel = input<string>('');
 
   valueChange = output<string>();
+  enter = output<void>();
 
   onInput(event: Event) {
     const inputElement = event.target as HTMLInputElement;
